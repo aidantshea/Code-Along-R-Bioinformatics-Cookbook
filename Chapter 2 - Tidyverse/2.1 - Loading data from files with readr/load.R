574b4ca4289901b653246781ce221f8e8c158f83
aidan <aidantshea@gmail.com>
2025-01-01 18:03:06 -0800

    completed 2.1

---
+++ Chapter 2 - Tidyverse/2.1 - Loading data from files with readr/load.R
@@ -0,0 +1,24 @@
+# This script loads in csv data from the 2021 UK census and saves it as a data frame. 
+
+# loading package dependencies
+library(readr)
+
+# generating file path to the census data
+filename <- fs::path_package("extdata", "census_2021.csv", package="rbioinfcookbook")
+
+col_names = c("area_code", "country", "region", "area_name", "all_persons", "ages_under_4",
+              paste0("ages_", seq(5, 85, by=5), "_to_", seq(9, 89, by=5)), 
+              "ages_over_90")
+
+# setting data types to columns of interest, defaulting other columns to numeric type
+col_types = cols(
+  area_code = col_character(), 
+  country = col_factor(levels = c("England", "Wales")),
+  region = col_factor(levels = c("North East", "Yorkshire Humber", "East Midlands", "West Midlands", "East of England", 
+                                 "London", "South East", "South West", "Wales"), ordered = TRUE),
+  area_name = col_character(),
+  .default = col_number()
+)
+
+# reading in data frame from .csv
+census_df <- read.csv(filename, skip = 7, col.names = col_names)
---
+++ Chapter 2 - Tidyverse/2.2 - Tidying a wide format table into a tidy table with tidyr/tidying1.R
@@ -0,0 +1,13 @@
+# TODO finish this script
+
+library(rbioinfcookbook)
+library(dplyr)
+library(tidyr)
+
+long_df <- census_df |>
+  rename("all_persons" = "population") |>
+  pivot_longer(
+    #
+    #
+    #
+  )
